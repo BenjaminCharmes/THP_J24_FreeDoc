1154c59be3a9b635f1c9f50fd5a5b1ab0bb92d11
BenjaminCharmes <benjamin.charmes@gmail.com>
2022-10-27 10:15:06 +0100

    Final commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  City.create(name: Faker::Address.city)
+end
+
+specialty = ["General practitioner", "Ophthalmologist", "Surgeon", "Physiotherapist", "Radiologist"]
+specialty.each do |specialty|
+  Specialty.create(specialty: specialty)
+end
+
+10.times do
+  Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Number.number(digits:5), city_id: rand(1..10))
+end
+
+20.times do
+  DoctorSpecialty.create(doctor_id: rand(0..10), specialty_id: rand(0..5))
+end
+
+50.times do
+  Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city_id: rand(1..10))
+end
+
+100.times do
+  Appointment.create(date: Faker::Time.between(from: DateTime.now, to: DateTime.now + 1, format: :default), doctor_id: rand(1..10) , patient_id: rand(1..50), city_id: rand(1..10))
+end
+
